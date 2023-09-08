ce4b55eae6dc833c065d3e3bc4e62bd93ef2276e
RAVI KIRAN <ravimsd.rv77@gmail.com>
2023-09-08 19:48:53 +0530

    Portfolio

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 $(document).ready(function () {
-  // Scroll down sticky navbar script start
+  
   $(window).scroll(function () {
     if (this.scrollY > 20) {
       $(".navbar").addClass("sticky");
     } else {
       $(".navbar").removeClass("sticky");
     }
-    // Scroll down sticky navbar script end
-
-    // Scroll up Button script start
+    
     if (this.scrollY > 500) {
       $(".scroll-up-btn").addClass("show");
     } else {
       $(".scroll-up-btn").removeClass("show");
     }
-    // Scroll up Button script end
-
-    // Fade In & Fade Out Elements on Scroll script start
+    
     $(".fadein").each(function (i) {
       var bottom_of_element = $(this).offset().top + $(this).outerHeight();
       var bottom_of_window = $(window).scrollTop() + $(window).height();
@@ -27,39 +23,35 @@ $(document).ready(function () {
       if (bottom_of_window < bottom_of_element) {
         $(this).removeClass("showme");
       }
-      // Fade In & Fade Out Elements on Scroll script end
+      
     });
   });
 
-  // Scroll up Button script start
+  
   $(".scroll-up-btn").click(function () {
     $("html").animate({ scrollTop: 0 });
   });
-  // Scroll up Button script end
-
-  // Typing animation script start
+  
   var typed = new Typed(".typing", {
-    strings: ["React.js Developer", "Front End Developer", "Youtuber", "Teacher"],
+    strings: ["React.js Developer", "Front End Developer", "Student"],
     typeSpeed: 100,
-    backSpeed: 60,
+    backSpeed: 80,
     loop: true,
   });
 
   var typed = new Typed(".typing2", {
-    strings: ["React.js Developer", "Front End Developer", "Youtuber", "Teacher"],
+    strings: ["React.js Developer", "Front End Developer", "Student"],
     typeSpeed: 100,
-    backSpeed: 60,
+    backSpeed: 80,
     loop: true,
   });
 
-  // toggle menu/navbar script start
+  
   $(".menu-btn").click(function () {
     $(".navbar .menu").toggleClass("active");
     $(".menu-btn i").toggleClass("active");
   });
-  // toggle menu/navbar script end
-
-  // owl carousel script start
+  
   $(".carousel").owlCarousel({
     margin: 20,
     loop: true,
@@ -80,5 +72,5 @@ $(document).ready(function () {
       },
     },
   });
-  // owl carousel script end
+  
 });
